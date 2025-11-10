b4aea103aa1b738aa9131577f5703d5f88d91136
alghazali <h1101231059@student.untan.ac.id>
2025-11-10 20:18:43 +0700

    package 3

--- config/database.php
+++ config/database.php
@@ -98,6 +98,7 @@ return [
             'sslmode' => 'prefer',
         ],
 
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
@@ -148,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
